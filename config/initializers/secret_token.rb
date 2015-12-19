adb312d7b8de3040681ae5da7018329fd700c01a
Robert Chasnouski <210954@student.pwr.wroc.pl>
2015-12-19 18:04:38 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Drugs::Application.config.secret_key_base = '939052053736c8b463b5e46d4341762d0c066c4954ed207be3e070339b2953f7b142d35fccc5f14af58f7f853876ccb6f4a6e51161269ef0143fd3f2fd9c0007'
